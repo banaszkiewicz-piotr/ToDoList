a6bfa1ddc165847b2e89537f41b0b41af2f23fba
banaszkiewicz-piotr <banaszkiewicz.piotr@zst.radom.pl>
2025-04-25 11:34:04 +0200

    link to the source

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -2,6 +2,8 @@ import TodoItem from './ToDoItem';
 import React from 'react';
 import { useState } from 'react';
 
+//source --> https://medium.com/@worachote/building-a-todo-list-app-with-reactjs-a-step-by-step-guide-2c58b9b6c0f5
+
 function ToDoList() {
     const [tasks, setTasks] = useState([
         {
